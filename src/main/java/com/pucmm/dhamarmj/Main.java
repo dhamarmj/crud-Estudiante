14462eefd956aad2994f258aa8ddf3a163fbe147
Dhamar Martínez <39349434+dhamarmj@users.noreply.github.com>
2019-03-06 20:32:50 -0300

    Agregando cambiar puerto

--- src/main/java/com/pucmm/dhamarmj/Main.java
+++ src/main/java/com/pucmm/dhamarmj/Main.java
@@ -1,7 +1,12 @@
 package com.pucmm.dhamarmj;
 
+import static spark.Spark.port;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        if(args.length >0)
+            port(Integer.parseInt(args[0]));
 
         new EstudianteHandler().estudianteHandler();
     }
